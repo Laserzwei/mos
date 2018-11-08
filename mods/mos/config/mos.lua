983a0ccaf9030267d3bab6941bcdcb5d42346a54
Laserzwei <thembaandcoproductions@gmx.de>
2018-11-08 13:29:00 +0100

    1.0.0 tag, will be changed, probably (0.19.0) -- removed complicated and dated way of "recreating" the asteroids in the target sector and replaced it with Galaxy():transferEntity() -- this makes spawningLib and Sector-change-Player-watchscript asteroidMover useless -- target sector can now be selected per asteroid. Gets instantly (and only) transferred, when "Pay & Move" is pressed -- sends chat message with coordinates to let you find your Asteroids again

--- mods/mos/config/mos.lua
+++ mods/mos/config/mos.lua
@@ -1,5 +1,15 @@
-config = {}
-config.VERSION = "[mOS][0.97b] "
+local config = {}
+config.Author = "Laserzwei"
+config.ModName = "Move Asteroids"
+config.version = {
+    major=1, minor=0, patch = 0,
+    string = function()
+        return  config.version.major .. '.' ..
+                config.version.minor .. '.' ..
+                config.version.patch
+    end
+}
+
 
 config.MONEY_PER_JUMP = 500000          --change to your needs
 config.CALLDISTANCE = 1500              --15Km is 1.500 not 15.000. Who made this up ?
--- mods/mos/scripts/entity/moveAsteroid.lua
+++ mods/mos/scripts/entity/moveAsteroid.lua
@@ -6,29 +6,37 @@ require ("stringutility")
 require ("faction")
 local config = require ("mods/mos/config/mos")
 
-MSSN = "isMarkedToMove"   --MoveStatuSaveName, gives the movestatus false,nil for not moving. true for needs to be moved
+
+local selectedSector = {}
+
+
 local window
+local selectSectorButton
 local payButton
 local transferToAllianceButton
 local permissions = {AlliancePrivilege.ManageStations, AlliancePrivilege.FoundStations, AlliancePrivilege.ModifyCrafts, AlliancePrivilege.SpendResources}
 local uiInitialized
 
+function initialize()
+    print(Entity().index.string, Entity().name)
+    Entity():registerCallback("onSectorEntered", "onSectorEntered")
+end
+
+-- changing position in target sector
+function onSectorEntered()
+    local x,y,z = math.random(-config.MAXDISPERSION, config.MAXDISPERSION), math.random(-config.MAXDISPERSION, config.MAXDISPERSION), math.random(-config.MAXDISPERSION, config.MAXDISPERSION)
+    Entity().translation = dvec3(x,y,z)
+end
+
 --is the player that tries to interact also the owner? Are we close enough? then return true.
 function interactionPossible(playerIndex, option)
     local player = Player(playerIndex)
-    local this = Entity()
-    if checkEntityInteractionPermissions(this, permissions) then
-        if this:getValue(MSSN) == nil then
-            unregisterAsteroid()
-        end
-
-        local craft = player.craft
-        if craft == nil then return false end
-
-        local dist = craft:getNearestDistance(this)
-
-        if dist < config.CALLDISTANCE then
-            prepUI()
+    local astro = Entity()
+    local craft = player.craft
+    if craft == nil then return false end
+    local dist = craft:getNearestDistance(astro)
+    if dist < config.CALLDISTANCE then
+        if checkEntityInteractionPermissions(astro, permissions) then
             return true
         end
     end
@@ -36,42 +44,35 @@ function interactionPossible(playerIndex, option)
 end
 
 function initUI()
-    --print(config.VERSION.."UI start")
+    --print(config.version.."UI start")
     local res = getResolution()
     local size = vec2(500, 300)
 
     local menu = ScriptUI()
     window = menu:createWindow(Rect(res * 0.5 - size * 0.5, res * 0.5 + size * 0.5))
-    if Entity():getValue(MSSN) then
-        window.caption = "Configure Movement of " --.. Entity().index.value
-    else
-        window.caption = "Move Asteroid " --.. Entity().index.value
-    end
+
     window.showCloseButton = 1
     window.moveable = 1
     window.closeableWithEscape = true
 
     window:createLabel(vec2(50, 10), "You will need "..createMonetaryString(config.MONEY_PER_JUMP).."Cr to jump this Asteroid.", 15)
     --botton pay
-    payButton = window:createButton(Rect(50, 200, 200, 30 + 200 ), "  Pay  ", "onPayPressed")
-    if Entity():getValue(MSSN) then
-        payButton.active = false
-    end
+    payButton = window:createButton(Rect(300, 200, 450, 30 + 200 ), "Pay & Move", "onPayPressed")
     payButton.maxTextSize = 15
 
     --button abort
-    local cancelBbutton = window:createButton(Rect(300, 200, 450, 30 + 200 ), " cancel Movement ", "onCancelPressed")
-    cancelBbutton.maxTextSize = 15
+    selectSectorButton = window:createButton(Rect(50, 200, 200, 30 + 200 ), "Select Sector", "onSectorSelectionPressed")
+    selectSectorButton.maxTextSize = 15
 
 
-    transferToAllianceButton = window:createButton(Rect(165, 150, 360, 30 + 150 ), " Transfer to Alliance ", "ontTransferPressed")
+    transferToAllianceButton = window:createButton(Rect(165, 150, 360, 30 + 150 ), "Transfer to Alliance", "onTransferOwnershipPressed")
     transferToAllianceButton.maxTextSize = 15
 
     if Player().allianceIndex then
         if Player().index == Entity().factionIndex then
-            transferToAllianceButton.caption = " Transfer to Alliance "
+            transferToAllianceButton.caption = "Transfer to Alliance "
         elseif Player().allianceIndex == Entity().factionIndex then
-            transferToAllianceButton.caption = " Transfer to You "
+            transferToAllianceButton.caption = "Transfer Ownership to You "
         end
         transferToAllianceButton.active = true
         transferToAllianceButton.visible = true
@@ -84,121 +85,78 @@ function initUI()
     uiInitialized = true
 end
 
-function prepUI()
-    if not uiInitialized then return end
-    if Player().allianceIndex then
-        transferToAllianceButton.active = true
-        transferToAllianceButton.visible = true
-    else
-        transferToAllianceButton.active = false
-        transferToAllianceButton.visible = false
-    end
-    if Entity():getValue(MSSN) == true then
-        window.caption = "Configure Asteroid Movement " --.. Entity().index.value
-        payButton.active = false
-        payButton.tooltip = "Already payed"
-    else
-        window.caption = "Move Asteroid " --.. Entity().index.value
-        payButton.active = true
-        payButton.tooltip = "No refunds!"
+-- find the selected Sector from GalaxyMap()
+function updateClient(timestep)
+    if uiInitialized then
+        local x, y =  GalaxyMap():getSelectedCoordinates()
+        if not (selectedSector.x == x and selectedSector.y == y) and not (x == 0 and y == 0) then
+            selectedSector.x, selectedSector.y = x,y
+            selectSectorButton.tooltip = "Selected Sector: ("..selectedSector.x..":"..selectedSector.y..")"
+        end
     end
 end
 
---playerIndex only available on Server
-function onCancelPressed(playerIndex)
-    if (onClient())then
-        invokeServerFunction("onCancelPressed",Player().index)
-        --print(config.VERSION.."Cancel Pressed ")
-        window.visible = false
-        return
+function onSectorSelectionPressed(playerIndex)
+    if onClient()then
+        GalaxyMap():show(Sector():getCoordinates())
     end
-    unregisterAsteroid()
-
-    --print(config.VERSION.."active Asteroid Movement cancelled on: ", Entity().index.value)
 end
 
 function onPayPressed()
-    if (onClient())then
-        invokeServerFunction("server_onPayPressed",Player().index)
-        --print(config.VERSION.."Pay Pressed ")
-        window.visible = false
-        return
-    else
-        --print(config.VERSION.."Pay Pressed on Server")
+    if onClient()then
+        if selectedSector.x and selectedSector.y then
+            invokeServerFunction("server_onPayPressed",Player().index, selectedSector)
+            window.visible = false
+        else
+            displayChatMessage("No sector selected!", "Asteroid", 1)
+        end
     end
-
 end
 
-function server_onPayPressed(playerIndex)
+function server_onPayPressed(playerIndex, selectedSector)
     local player = Player(playerIndex)
     local owner = checkEntityInteractionPermissions(Entity(), permissions)
     if owner then
-        local isMarkedToMove = Entity():getValue(MSSN)
         local canPay, msg, args = owner:canPay(config.MONEY_PER_JUMP)
-
-        if canPay and (isMarkedToMove == false or isMarkedToMove == nil) then
-            owner:pay("",config.MONEY_PER_JUMP)
-            registerAsteroid()
-            --print(config.VERSION..tostring(owner.name).." payed for Asteroid moving")
+        if canPay then
+            local x,y = Sector():getCoordinates()
+            if selectedSector.x and selectedSector.y then
+                if not (selectedSector.x == x and selectedSector.y == y) then
+                    owner:pay("",config.MONEY_PER_JUMP)
+                    Galaxy():transferEntity(Entity(), selectedSector.x, selectedSector.y, 1)
+                    player:sendChatMessage("Asteroid", 0, [[Asteroid has been transferred to sector \s(%s:%s) !]], selectedSector.x, selectedSector.y)
+                else
+                    player:sendChatMessage("Asteroid", 1, "Asteroid is already in sector ("..x..":"..y..") !")
+                end
+            else
+                player:sendChatMessage("Asteroid", 1, "No sector selected!")
+            end
         else
             player:sendChatMessage("Asteroid", 1, msg,unpack(args))
             return
         end
     else
-        --print(config.VERSION.."Pay pressed server answer by wrong player:".. Player().name .. " | from: " ..Player(playerIndex).name )
+        --print(config.version.."Pay pressed server answer by wrong player:".. Player().name .. " | from: " ..Player(playerIndex).name )
         return
     end
 end
 
-function ontTransferPressed()
-    invokeServerFunction("server_ontTransferPressed", Player().index)
+function onTransferOwnershipPressed()
+    invokeServerFunction("server_onTransferOwnershipPressed", Player().index)
     if Player().index == Entity().factionIndex then
-        transferToAllianceButton.caption = " Transfer to You "
+        transferToAllianceButton.caption = "Transfer Ownership to You "
     elseif Player().allianceIndex == Entity().factionIndex then
-        transferToAllianceButton.caption = " Transfer to Alliance "
+        transferToAllianceButton.caption = "Transfer to Alliance "
     end
     window.visible = false
 end
 
-function server_ontTransferPressed(playerIndex)
-    if callingPlayer == playerIndex then
-        if Player(callingPlayer).allianceIndex == Entity().factionIndex then
-            printlog("transferred Asteroid ".. Entity().index.value .. " to Player" ..Player(callingPlayer).name)
-            Entity().factionIndex = callingPlayer
-        else
-            printlog("transferred Asteroid ".. Entity().index.value .. " to Alliance " ..Alliance(Player(callingPlayer).allianceIndex).name)
-            Entity().factionIndex = Player(callingPlayer).allianceIndex
-        end
-    end
-end
-
-function registerAsteroid()
-    if onServer() then
-        local scripts = Entity():getScripts()
-        local minefounderRunning = false
-        local sellobjectRunning = false
-        for _,script in pairs(scripts) do
-            if script == "data/scripts/entity/minefounder.lua" then
-                minefounderRunning = true
-            end
-            if script == "data/scripts/entity/sellobject.lua" then
-                sellobjectRunning = true
-            end
-        end
-        if minefounderRunning == true then
-            Entity():removeScript("data/scripts/entity/minefounder.lua")
-        end
-        if sellobjectRunning == true then
-            Entity():removeScript("data/scripts/entity/sellobject.lua")
-        end
-        Entity():setValue(MSSN,true)
-    end
-end
-
-function unregisterAsteroid()
-    if onServer() then
-        Entity():addScriptOnce("data/scripts/entity/minefounder.lua")
-        Entity():addScriptOnce("data/scripts/entity/sellobject.lua")
-        Entity():setValue(MSSN,false)
+function server_onTransferOwnershipPressed(playerIndex)
+    if Player(callingPlayer).allianceIndex == Entity().factionIndex then
+        printlog("transferred Asteroid ".. Entity().index.value .. " to Player" ..Player(callingPlayer).name)
+        Entity().factionIndex = callingPlayer
+    else
+        printlog("transferred Asteroid ".. Entity().index.value .. " to Alliance " ..Alliance(Player(callingPlayer).allianceIndex).name)
+        Entity().factionIndex = Player(callingPlayer).allianceIndex
     end
 end
